f3bbd29324c87a3623014613b1d9ccd54ccb3415
Kaspar Schleiser <kaspar@schleiser.de>
2022-09-12 11:32:11 +0200

    scripts/murdock-worker.sh: override git.riot-os.org:3333 url ssh -> https

--- scripts/murdock-worker.sh
+++ scripts/murdock-worker.sh
@@ -11,4 +11,7 @@ export HOME=/data/riotbuild
 # initialize git cache, bail out if it won't work
 git-cache init || exit 0
 
+# use https instead of ssh for git.riot-os.org
+git config --global url.https://git.riot-os.org/.insteadOf ssh://git.riot-os.org:3333/
+
 exec $*
